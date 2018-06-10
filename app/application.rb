3c15f8abb8765faaa227222aab477e456d64ae65
Alex Wilson <fonzmp@gmail.com>
2018-06-10 16:53:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -43,7 +43,7 @@ class Application
   def add_item(search_term)
     if @@items.include?(search_term)
       @@cart << search_term
-      resp.write "Added #{search_term}"
+      return "Added #{search_term}"
     else
       resp.write "We don't have that item"
     end
